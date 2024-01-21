0cfc630ba459bffdd6befb307cd10bb0c0292c90
shakerkamal <shakerkamal18@gmail.com>
2024-01-21 23:55:12 +0100

    Unit tests are written for products controller methods

--- backend/EShop/EShop.Test/Controllers/ProductsControllerTests.cs
+++ backend/EShop/EShop.Test/Controllers/ProductsControllerTests.cs
@@ -1,7 +1,14 @@
 ﻿using EShop.Api.Controllers;
+using EShop.Application.Commands.CreateProduct;
+using EShop.Application.Commands.DeleteProduct;
+using EShop.Application.Commands.UpdateProduct;
 using EShop.Application.Queries.GetProduct;
+using EShop.Application.Queries.GetProducts;
+using EShop.Contracts;
 using EShop.Shared.DataTransferObjects.ProductDtos;
+using EShop.Shared.RequestFeatures;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 
@@ -21,11 +28,42 @@ public class ProductsControllerTests
     }
 
     [Fact]
+    public async Task GetProducts_WhenCalled_ReturnsPagedListOfProducts()
+    {
+        // Arrange
+        var pagedProduct = new PagedProduct
+        {
+            PageNumber = 1,
+            PageSize = 10
+        };
+
+        _senderMock.Setup(x => x.Send(It.IsAny<GetProductsQuery>(), default))
+                  .ReturnsAsync(new PaginatedList<ProductIndexDto>(5, 1, 5, new List<ProductIndexDto>()
+                  {
+                      new ProductIndexDto("1", "Xbox", string.Empty, "Microsoft", "Gaming console", 200, 450, 12),
+                      new ProductIndexDto("2", "Xbox2", string.Empty, "Microsoft", "Gaming console", 100, 650, 32),
+                      new ProductIndexDto("3", "Xbox3", string.Empty, "Microsoft", "Gaming console", 200, 450, 12),
+                      new ProductIndexDto("4", "Xbox4", string.Empty, "Microsoft", "Gaming console", 100, 650, 32),
+                      new ProductIndexDto("5", "Xbox5", string.Empty, "Microsoft", "Gaming console", 200, 450, 12),
+                  }));
+
+        _controller.ControllerContext = new ControllerContext
+        {
+            HttpContext = new DefaultHttpContext()
+        };
+
+        // Act
+        var result = await _controller.GetProducts(pagedProduct);
+
+        // Assert
+        Assert.IsType<OkObjectResult>(result);
+        _senderMock.Verify(x => x.Send(It.IsAny<GetProductsQuery>(), default), Times.Once);
+    }
+
+    [Fact]
     public async Task GetProduct_WhenCalled_ReturnsOneProduct()
     {
         // Arrange
-        //var senderMock = new Mock<ISender>();
-        //var controller = new ProductsController(senderMock.Object);
         var productId = "1";
 
         _senderMock.Setup(x => x.Send(It.IsAny<GetProductDetailsQuery>(), It.IsAny<CancellationToken>()))
@@ -37,5 +75,57 @@ public class ProductsControllerTests
         // Assert
         Assert.IsType<OkObjectResult>(result);
         _senderMock.Verify(x => x.Send(It.IsAny<GetProductDetailsQuery>(), It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task CreateProduct_ReturnsOkResult()
+    {
+        // Arrange
+        var productDto = new CreateProductDto("User1", "Xbox", string.Empty, "Microsoft", "Gaming console", "Gaming console for playinh games", null, 3.9, 200, 450, 12);
+
+        _senderMock.Setup(x => x.Send(It.IsAny<CreateProductCommand>(), It.IsAny<CancellationToken>()))
+                  .ReturnsAsync(new ProductIndexDto("1", "Xbox", string.Empty, "Microsoft", "Gaming console", 200, 450, 12));
+
+        // Act
+        var result = await _controller.CreateProduct(productDto);
+
+        // Assert
+        Assert.IsType<OkObjectResult>(result);
+        _senderMock.Verify(x => x.Send(It.IsAny<CreateProductCommand>(), It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task UpdateProduct_ReturnsNoContentResult()
+    {
+        // Arrange
+        var productId = "1";
+        var updateProduct = new UpdateProductDto("User1", "Xbox", string.Empty, "Microsoft", "Gaming console", "Gaming console for playinh games", null, 3.9, 200, 450, 12);
+
+        _senderMock.Setup(x => x.Send(It.IsAny<UpdateProductCommand>(), It.IsAny<CancellationToken>()))
+                  .Returns(Task.CompletedTask);
+
+        // Act
+        var result = await _controller.UpdateProduct(productId, updateProduct);
+
+        // Assert
+        Assert.IsType<NoContentResult>(result);
+        _senderMock.Verify(x => x.Send(It.IsAny<UpdateProductCommand>(), It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task DeleteProduct_ReturnsNoContentResult()
+    {
+        // Arrange
+        var productId = "1";
+
+        _senderMock.Setup(x => x.Send(It.IsAny<DeleteProductCommand>(), It.IsAny<CancellationToken>()))
+                  .Returns(Task.CompletedTask);
+
+        // Act
+        var result = await _controller.DeleteProduct(productId);
+
+        // Assert
+        Assert.IsType<NoContentResult>(result);
+        _senderMock.Verify(x => x.Send(It.IsAny<DeleteProductCommand>(), It.IsAny<CancellationToken>()), Times.Once);
     }
 }
